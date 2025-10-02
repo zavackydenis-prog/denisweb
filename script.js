c0d31c324e2e9085259ddce1c0101fcf6cd00726
zavackydenis-prog <zavackydenis@gmail.com>
2025-10-02 17:31:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hamburger menu
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
@@ -5,4 +6,12 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
 
+// Voliteľne: zvýraznenie aktívnej položky menu
+const currentLocation = window.location.pathname;
+const menuItems = document.querySelectorAll('nav ul li a');
 
+menuItems.forEach(link => {
+  if (link.getAttribute('href') === currentLocation.split('/').pop()) {
+    link.classList.add('active');
+  }
+});
