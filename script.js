29e4e261b61bd57f466bf65d6ea1f1e8ed144e00
zavackydenis-prog <zavackydenis@gmail.com>
2025-10-03 18:47:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navLinks = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+  const forms = document.querySelectorAll("form");
+
+  forms.forEach(form => {
+    form.addEventListener("submit", (e) => {
+      // tu máš už asi odosielanie cez mail handler
+      setTimeout(() => {
+        form.reset(); // vymaže všetky polia
+      }, 500);
+    });
+  });
+});
+</script>
+
